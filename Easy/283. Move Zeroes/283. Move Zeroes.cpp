e42ae403ceb0e513d48ae41090500d986549963a
YuJenW <r387y95a15n@gmail.com>
2026-05-12 15:15:54 +0800

    Create 283. Move Zeroes.cpp

---
+++ Easy/283. Move Zeroes/283. Move Zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int slow = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                if (i != slow) {
+                    swap(nums[i], nums[slow]);
+                }
+                slow++;
+            }
+        }
+    }
+};
