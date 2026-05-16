202610634cef501945f798dcda2ced9e34ba6c3d
YuJenW <r387y95a15n@gmail.com>
2026-05-16 22:58:36 +0800

    Update 443. String Compression.cpp

--- Mid_LeveL/443. String Compression/443. String Compression.cpp
+++ Mid_LeveL/443. String Compression/443. String Compression.cpp
@@ -1,1 +1,38 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int write = 0;
+        int read = 0;
+        int n = chars.size();
 
+        while (read < n) {
+            char currentChar = chars[read];
+            int count = 0;
+
+            // 計算目前字元連續出現幾次
+            while (read < n && chars[read] == currentChar) {
+                read++;
+                count++;
+            }
+
+            // 寫入字元
+            chars[write++] = currentChar;
+
+            // 如果 count > 1，將 count 拆成每一位 digit 寫入
+            if (count > 1) {
+                int start = write;
+
+                // 先反向寫入個位、十位、百位...
+                while (count > 0) {
+                    chars[write++] = '0' + (count % 10); //ascii to char
+                    count /= 10;
+                }
+
+                // 因為剛剛是反向寫入，所以需要反轉回來
+                reverse(chars.begin() + start, chars.begin() + write);
+            }
+        }
+
+        return write;
+    }
+};
